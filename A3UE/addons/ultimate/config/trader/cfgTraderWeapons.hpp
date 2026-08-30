df927111041c5bf5c5101f3fb07a8accbdd8ec42
Sven van Elswijk <svenfire99@gmail.com>
2026-08-30 02:46:37 +0200

    config trader

--- A3UE/addons/ultimate/config/trader/cfgTraderWeapons.hpp
+++ A3UE/addons/ultimate/config/trader/cfgTraderWeapons.hpp
@@ -1,16 +1,19 @@
-        class weapons_base
-        {
-            prefix = "";
-        };
-        class weapons_Tier1 : weapons_base
-        {
-            prefix = "Tier1_stock"
-        };
-        class weapons_SIGXM7 : weapons_base
-        {
-            prefix = "SIGXM7_stock"
-        };
-        class weapons_mss : weapons_base
-        {
-            prefix = "mss";
-        };
+class weapons_base
+{
+    prefix = "";
+};
+
+class weapons_Tier1 : weapons_base
+{
+    prefix = "Tier1_stock";
+};
+
+class weapons_SIGXM7 : weapons_base
+{
+    prefix = "SIGXM7_stock";
+};
+
+class weapons_mss : weapons_base
+{
+    prefix = "mss";
+};
